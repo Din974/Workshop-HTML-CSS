44e2907aee4d929aedc099ee131062b868f9ee9f
luidgi parinet <luidgi.parinet@epitech.eu>
2021-02-10 12:35:20 +0400

    push workshop

---
+++ front/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <HeaderBar/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import HeaderBar from '@/components/HeaderBar.vue';
+export default {
+  components: {
+    HeaderBar,
+  },
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,900&display=swap');
+</style>
---
+++ front/src/components/HeaderBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="HeaderBar">
+        <h1>PostApp</h1>
+        <ul>
+            <li>Home</li>
+            <li><router-link to="/login">Login</router-link></li>
+            <li>Sign up</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .HeaderBar {
+        background-color: blue;
+        display: flex;
+        justify-content: space-around;
+    }
+    ul {
+        display: flex;
+        justify-content: space-around;
+        width: 300px;
+    }
+</style>
